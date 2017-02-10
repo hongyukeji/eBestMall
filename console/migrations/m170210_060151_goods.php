5d24b9b555395aa713ad9dbaba38301923a7d00d
Shadow <admin@hongyuvip.com>
2017-02-10 18:20:47 +0800

    eBestMall

--- console/migrations/m170210_060151_goods.php
+++ console/migrations/m170210_060151_goods.php
@@ -15,7 +15,17 @@ class m170210_060151_goods extends Migration
         }
 
         $this->createTable(self::TABLE_NAME, [
-            'id' => $this->primaryKey(),
+            'goodsId' => $this->primaryKey(),
+            'goodsName' => $this->string()->notNull()->comment('商品名称'),
+            'goodsBrief' => $this->string()->comment('商品简介'),
+            'goodsPrice' => $this->decimal(10,2)->defaultValue('0.00')->comment('商品价格'),
+            'goodsMarketPrice' => $this->decimal(10,2)->defaultValue('0.00')->comment('商品市场价格'),
+            'goodsNumber' => $this->integer()->defaultValue(0)->comment('商品数量'),
+            'goodsSalesVolume' => $this->integer()->defaultValue(0)->comment('商品销量'),
+            'goodsIntroduce' => $this->text()->comment('商品介绍'),
+            'goodsStatus' => $this->smallInteger(1)->comment('状态:1-显示,0-关闭'),
+            'createdTime' => $this->integer()->comment('创建时间'),
+            'updatedTime' => $this->integer()->comment('更新时间'),
         ], $tableOptions);
     }
 
