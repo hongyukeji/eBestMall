f4bcd02de5cce674efe7516852080d1b7f1766bc
Shadow <admin@hongyuvip.com>
2017-02-11 15:29:01 +0800

    eBestMall

--- console/migrations/m170210_060200_order.php
+++ console/migrations/m170210_060200_order.php
@@ -20,7 +20,7 @@ class m170210_060200_order extends Migration
             'AddressId' => $this->integer()->notNull()->defaultValue('0')->comment('收货地址'),
             'orderAmount' => $this->decimal(10,2)->notNull()->defaultValue('0.00')->comment('订单总价'),
             'shippingId' => $this->integer()->notNull()->defaultValue('0')->comment('配送方式'),
-            'status' => $this->smallInteger()->defaultValue('0')->comment('状态:1-开启,0-关闭'),
+            'status' => $this->smallInteger()->defaultValue('0')->comment('订单状态:1-已完成,0-未完成'),
             'createdTime' => $this->integer()->comment('创建时间'),
             'updatedTime' => $this->integer()->comment('更新时间'),
         ], $tableOptions);
