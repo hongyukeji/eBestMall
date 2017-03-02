19949a8980267b1cce635fa3fcf1dfc45ca07f8f
Shadow <admin@hongyuvip.com>
2017-03-03 01:50:17 +0800

    eBestMall

--- common/config/main.php
+++ common/config/main.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'name' => 'eBestMall',  // 系统名称
-    'version' => '1.0', // 系统版本
+    'version' => '1.0.0', // 系统版本
     'language'=>'zh-CN',    // 设置语言
     'timeZone'=>'Asia/Shanghai',    // 设置时区
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
