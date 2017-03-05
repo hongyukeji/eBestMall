4dd4dafc2d27a9440c828b6efba2e4c61eba6eeb
Shadow <admin@hongyuvip.com>
2017-03-05 18:40:41 +0800

    eBestMall

--- frontend/themes/default/site/login.php
+++ frontend/themes/default/site/login.php
@@ -2,7 +2,7 @@
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
-/* @var $model \common\models\LoginForm */
+/* @var $model \frontend\models\LoginForm */
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
