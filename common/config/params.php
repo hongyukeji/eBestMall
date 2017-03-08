de4a107211c2b89619ca183730df688d6102fe54
Shadow <admin@hongyuvip.com>
2017-03-08 15:52:00 +0800

    eBestMall

--- common/config/params.php
+++ common/config/params.php
@@ -1,6 +1,10 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
+    'shopName' => 'eBestMall',
+    'shopTitle' => 'eBestMall - B2B2C商城系统',
+    'keywords' => 'eBestMall,鸿宇商城,鸿宇分销,网店系统,网上商城系统,分销系统,B2B2C商城系统,微信分销系统',
+    'description' => 'eBestMall是国内电子商务系统及服务解决方案新创品牌。为传统企业及创业者提供零售网店系统、网上商城系统、分销系统、B2B2C商城系统、微信分销系统、行业ERP等产品和解决方案。',
+    'adminEmail' => 'admin@hongyuvip.com',
+    'supportEmail' => 'ebestmall@126.com',
     'user.passwordResetTokenExpire' => 3600,
 ];
