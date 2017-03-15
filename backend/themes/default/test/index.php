1d5fd16afa19e0e146270f73f8e762ee0293e650
Shadow <admin@hongyuvip.com>
2017-03-15 19:23:07 +0800

    eBestMall

--- backend/themes/default/test/index.php
+++ backend/themes/default/test/index.php
@@ -12,7 +12,33 @@
  * ============================================================================
  */
 
+$this->title = Yii::t('common', 'Test');
+$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="row">
-    <h1>The is Test</h1>
+<div class="test-index">
+
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+        Launch demo modal
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
