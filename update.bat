7c8e21821b517fa72a42651e18c8aad7c02bebf2
Shadow <admin@hongyuvip.com>
2017-03-16 02:23:32 +0800

    eBestMall

---
+++ update.bat
@@ -0,0 +1,16 @@
+@echo off
+
+rem -------------------------------------------------------------
+rem  eBestMall command line bootstrap script for Windows.
+rem  @author Shadow <admin@hongyuvip.com>
+rem  @link http://www.ebestmall.com/
+rem  @copyright Copyright (c) 2017 eBestMall Software LLC
+rem -------------------------------------------------------------
+
+@setlocal
+
+call composer update
+
+pause
+
+@endlocal
