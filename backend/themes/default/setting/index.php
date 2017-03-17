6d5a57c174c89ebb1b9b0a23f652b2727c1abb00
Shadow <admin@hongyuvip.com>
2017-03-17 16:33:20 +0800

    eBestMall

--- backend/themes/default/setting/index.php
+++ backend/themes/default/setting/index.php
@@ -15,22 +15,94 @@ $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 <div class="setting-index">
+    <div class="body-content">
+        <div class="row">
+            <div class="col-md-12">
+                <!-- Custom Tabs -->
+                <div class="nav-tabs-custom">
+                    <ul class="nav nav-tabs">
+                        <li class="active"><a href="#tab_1" data-toggle="tab" aria-expanded="true">基本设置</a></li>
+                        <li class=""><a href="#tab_2" data-toggle="tab" aria-expanded="false">显示设置</a></li>
+                        <li><a href="#tab_3" data-toggle="tab">支付设置</a></li>
+                        <li><a href="#tab_4" data-toggle="tab">配送方式</a></li>
+                        <li><a href="#tab_5" data-toggle="tab">短信设置</a></li>
+                        <li><a href="#tab_5" data-toggle="tab">入驻商设置</a></li>
+                    </ul>
+                    <div class="tab-content">
+                        <div class="tab-pane active" id="tab_1">
+                            <?php $form = ActiveForm::begin(); ?>
 
-    <?php $form = ActiveForm::begin(); ?>
+                            <?= $form->field($model, 'name') ?>
 
-        <?= $form->field($model, 'name') ?>
+                            <?= $form->field($model, 'title') ?>
 
-        <?= $form->field($model, 'title') ?>
+                            <?= $form->field($model, 'keywords') ?>
 
-        <?= $form->field($model, 'keywords') ?>
+                            <?= $form->field($model, 'description')->textarea() ?>
 
-        <?= $form->field($model, 'description')->textarea() ?>
+                            <?= $form->field($model, 'copyright') ?>
 
-        <?= $form->field($model, 'copyright') ?>
-
-        <div class="form-group">
-            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+                            <div class="form-group">
+                                <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+                            </div>
+                            <?php ActiveForm::end(); ?>
+                        </div>
+                        <!-- /.tab-pane -->
+                        <div class="tab-pane" id="tab_2">
+                            The European languages are members of the same family. Their separate existence is a myth.
+                            For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ
+                            in their grammar, their pronunciation and their most common words. Everyone realizes why a
+                            new common language would be desirable: one could refuse to pay expensive translators. To
+                            achieve this, it would be necessary to have uniform grammar, pronunciation and more common
+                            words. If several languages coalesce, the grammar of the resulting language is more simple
+                            and regular than that of the individual languages.
+                        </div>
+                        <!-- /.tab-pane -->
+                        <div class="tab-pane" id="tab_3">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            It has survived not only five centuries, but also the leap into electronic typesetting,
+                            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+                            sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+                            like Aldus PageMaker including versions of Lorem Ipsum.
+                        </div>
+                        <!-- /.tab-pane -->
+                        <div class="tab-pane" id="tab_4">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            It has survived not only five centuries, but also the leap into electronic typesetting,
+                            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+                            sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+                            like Aldus PageMaker including versions of Lorem Ipsum.
+                        </div>
+                        <!-- /.tab-pane -->
+                        <div class="tab-pane" id="tab_5">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            It has survived not only five centuries, but also the leap into electronic typesetting,
+                            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+                            sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+                            like Aldus PageMaker including versions of Lorem Ipsum.
+                        </div>
+                        <!-- /.tab-pane -->
+                        <div class="tab-pane" id="tab_6">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            It has survived not only five centuries, but also the leap into electronic typesetting,
+                            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+                            sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+                            like Aldus PageMaker including versions of Lorem Ipsum.
+                        </div>
+                        <!-- /.tab-pane -->
+                    </div>
+                    <!-- /.tab-content -->
+                </div>
+                <!-- nav-tabs-custom -->
+            </div>
         </div>
-    <?php ActiveForm::end(); ?>
-
+    </div>
 </div><!-- setting-index -->
