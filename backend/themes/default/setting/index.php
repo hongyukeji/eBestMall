aa6e20fdc5821e0238ca1008ce78ae1dc8380d01
Shadow <admin@hongyuvip.com>
2017-03-17 20:47:46 +0800

    eBestMall

--- backend/themes/default/setting/index.php
+++ backend/themes/default/setting/index.php
@@ -26,7 +26,7 @@ $fieldOptions = [
                 <div class="nav-tabs-custom">
                     <ul class="nav nav-tabs">
                         <li class="active"><a href="#tab_1" data-toggle="tab" aria-expanded="true"><?= Yii::t('common', 'Basic').Yii::t('common', 'Setting') ?></a></li>
-                        <li><a href="#tab_2" data-toggle="tab" aria-expanded="false">显示设置</a></li>
+                        <li><a href="#tab_2" data-toggle="tab" aria-expanded="false">联系方式</a></li>
                     </ul>
                     <div class="tab-content">
                         <div class="tab-pane active" id="tab_1">
@@ -58,7 +58,7 @@ $fieldOptions = [
                         </div>
                         <!-- /.tab-pane -->
                         <div class="tab-pane" id="tab_2">
-                            <div class="box-body">显示设置</div>
+                            <div class="box-body">联系方式</div>
                         </div>
                         <!-- /.tab-pane -->
                     </div>
