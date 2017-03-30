9bec9b8545abbf24436b4b808f855123b052d939
Shadow <admin@hongyuvip.com>
2017-03-30 12:54:55 +0800

    eBestMall

--- frontend/modules/mobile/controllers/DefaultController.php
+++ frontend/modules/mobile/controllers/DefaultController.php
@@ -15,6 +15,7 @@ class DefaultController extends Controller
      */
     public function actionIndex()
     {
+        $this->layout = 'main-mobile';
         return $this->render('index');
     }
 }
--- frontend/modules/mobile/views/default/index.php
+++ frontend/modules/mobile/views/default/index.php
@@ -1,120 +1,30 @@
-<!DOCTYPE html>
-<html lang="zh-CN">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <meta name="format-detection" content="telephone=no">
-    <meta http-equiv="Expires" content="-1">
-    <meta http-equiv="Cache-Control" content="no-cache">
-    <meta http-equiv="Pragma" content="no-cache">
-    <meta name="author" content="www.ebestmall.com">
+<?php
+use ebestmall\web\EbmMobileAsset;   // 引入 EbmMobileAsset 资源包
+EbmMobileAsset::register($this);  // 在本视图注册此资源包
+$baseUrl = $this->assetBundles[EbmMobileAsset::className()]->baseUrl;   // 获取发布后资源包对应的临时目录
 
-    <title>eBestMall - B2B2C商城系统</title>
-    <meta name="description" content="eBestMall是国内电子商务系统及服务解决方案新创品牌。为传统企业及创业者提供零售网店系统、网上商城系统、分销系统、B2B2C商城系统、微信分销系统、行业ERP等产品和解决方案。" />
-    <meta name="keyword" content="eBestMall,鸿宇商城,鸿宇分销,网店系统,网上商城系统,分销系统,B2B2C商城系统,微信分销系统" />
+//$this->registerCssFile($baseUrl .'/css/index.css', ['depends' => EbmMobileAsset::className()]); // 视图引用单独文件示例
+$this->registerJsFile($baseUrl .'/js/index.js',['depends' => EbmMobileAsset::className()]);
 
-    <link rel="stylesheet" href="static/css/app.css" />
-</head>
-<body>
-<div class="header">
-    <div class="header-item"></div>
-    <div class="header-wrap">
-        <div class="header-logo">
-            <div class="header-logo-wrap">
-                <span></span>
-            </div>
-        </div>
-        <div class="header-search">
-            <div class="header-search-wrap">
-                <form action="javascript:;">
-                    <input type="text" placeholder="搜索商品/店铺">
-                    <button type="submit" class="icon-search2"></button>
-                </form>
-            </div>
-        </div>
-        <div class="header-login"><a href="javascript:;">登录</a></div>
-        <!-- 登陆后显示图标 -->
-        <!--<div class="header-login"><a href="javascript:;"><i class="icon-user-o"></i></a></div>-->
-    </div>
-</div>
-
-<div class="content">
-    <div class="content-wrap">
-        <div class="slide"></div>
-        <div class="shortcut-menu">
-            <ul>
-                <li><a href="javascript:;">团购</a></li>
-                <li><a href="javascript:;">一元购</a></li>
-                <li><a href="javascript:;">秒杀</a></li>
-                <li><a href="javascript:;">本地生活</a></li>
-                <li><a href="javascript:;">众筹</a></li>
-
-                <li><a href="javascript:;">团购</a></li>
-                <li><a href="javascript:;">一元购</a></li>
-                <li><a href="javascript:;">秒杀</a></li>
-                <li><a href="javascript:;">本地生活</a></li>
-                <li><a href="javascript:;">众筹</a></li>
-
-            </ul>
-        </div>
-
-        <div class="returnTop"><a href="javascript:;"><i class="icon-arrow-up"></i></a></div>
-    </div>
-</div>
-
-<div class="footer">
-    <div class="footer-wrap">
+?>
+<div class="content-wrap">
+    <div class="slide"></div>
+    <div class="shortcut-menu">
         <ul>
-            <li>
-                <a href="javascript:;">
-                    <div class="footer-menu-icon">
-                        <i class="icon-home3"></i>
-                    </div>
-                    <div class="footer-menu-title">首页</div>
-                </a>
-            </li>
-            <li>
-                <a href="javascript:;">
-                    <div class="footer-menu-icon">
-                        <i class="icon-star-o"></i>
-                    </div>
-                    <div class="footer-menu-title">分类</div>
-                </a>
-            </li>
-            <li>
-                <a href="javascript:;">
-                    <div class="footer-menu-icon">
-                        <i class="icon-search2"></i>
-                    </div>
-                    <div class="footer-menu-title">发现</div>
-                </a>
-            </li>
-            <li>
-                <a href="javascript:;">
-                    <div class="footer-menu-icon">
-                        <i class="icon-cart"></i>
-                    </div>
-                    <div class="footer-menu-title">购物车</div>
-                </a>
-            </li>
-            <li>
-                <a href="javascript:;">
-                    <div class="footer-menu-icon">
-                        <i class="icon-user-o"></i>
-                    </div>
-                    <div class="footer-menu-title">我的</div>
-                </a>
-            </li>
+            <li><a href="javascript:;">团购</a></li>
+            <li><a href="javascript:;">一元购</a></li>
+            <li><a href="javascript:;">秒杀</a></li>
+            <li><a href="javascript:;">本地生活</a></li>
+            <li><a href="javascript:;">众筹</a></li>
+
+            <li><a href="javascript:;">团购</a></li>
+            <li><a href="javascript:;">一元购</a></li>
+            <li><a href="javascript:;">秒杀</a></li>
+            <li><a href="javascript:;">本地生活</a></li>
+            <li><a href="javascript:;">众筹</a></li>
+
         </ul>
     </div>
+
+    <div class="returnTop"><a href="javascript:;"><i class="icon-arrow-up"></i></a></div>
 </div>
-
-<script type="text/javascript" src="static/js/jquery.min.js"></script>
-<script type="text/javascript" src="static/js/app.js"></script>
-<script type="text/javascript" src="static/js/index.js"></script>
-
-</body>
-</html>
