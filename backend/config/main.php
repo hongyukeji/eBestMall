d5ac5031c70914643c7d654cd2b69b19da7138e1
Shadow <admin@hongyuvip.com>
2017-05-11 23:40:59 +0800

    eBestMall

--- backend/config/main.php
+++ backend/config/main.php
@@ -75,7 +75,7 @@ return [
         ],
         'session' => [
             // this is the name of the session cookie used for login on the backend
-            'name' => 'advanced-backend',
+            'name' => 'eBestMall-backend',
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
