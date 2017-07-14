9489a0eb2212df197f455a1ce9e8003a33ae458a
Shadow <admin@hongyuvip.com>
2017-07-14 14:04:37 +0800

    eBestMall

--- frontend/themes/default/layouts/head.php
+++ frontend/themes/default/layouts/head.php
@@ -5,6 +5,6 @@
     <meta name="renderer" content="webkit">
     <?= \yii\helpers\Html::csrfMetaTags() ?>
     <title><?= \yii\helpers\Html::encode($this->title) ?></title>
-    <link rel="shortcut icon" href="<?= Yii::$app->homeUrl ?>/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="<?= Yii::$app->request->getHostInfo() ?>/favicon.ico" type="image/x-icon">
     <?php $this->head() ?>
 </head>
