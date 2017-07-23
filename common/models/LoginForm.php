fc2a6a8f00e81f0eb0936352b8edb3c06598fdf0
Shadow <admin@hongyuvip.com>
2017-07-23 15:37:41 +0800

    eBestMall

--- common/models/LoginForm.php
+++ common/models/LoginForm.php
@@ -58,7 +58,7 @@ class LoginForm extends Model
     {
         if ($this->validate()) {
             $this->updateRecord($this->getUser()->id);    // 登录后触发记录事件
-            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
+            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 1 : 0);
         } else {
             return false;
         }
