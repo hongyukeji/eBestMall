7f35e5aa979f9deae0102522a7011990c02989ba
Shadow <admin@hongyuvip.com>
2017-09-01 00:56:31 +0800

    eBestMall - Laravel 5.5 LTS

---
+++ config/system.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'name' => 'eBestMall',
+    'version' => '1.0.0',
+    'url' => 'www.ebestmall.com',
+    'install_time' => '0000-00-00',
+    'update_time' => '0000-00-00',
+];
