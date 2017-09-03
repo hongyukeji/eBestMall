963b47b9eb529712dbc499b75b983cbd8a833a83
Shadow <admin@hongyuvip.com>
2017-09-03 11:38:57 +0800

    eBestMall - Laravel 5.5 LTS

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,83 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="renderer" content="webkit">
+    <title>eBestMall - B2B2C商城系统</title>
+    <meta name="description" content="eBestMall是国内电子商务系统及服务解决方案新创品牌。为传统企业及创业者提供零售网店系统、网上商城系统、分销系统、B2B2C商城系统、微信分销系统、行业ERP等产品和解决方案。">
+    <meta name="keyword" content="eBestMall,鸿宇商城,鸿宇分销,网店系统,网上商城系统,分销系统,B2B2C商城系统,微信分销系统">
+    <link rel="stylesheet" href="/static/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/static/css/app.css" />
+    <link rel="stylesheet" href="/static/css/login.css" />
+<body>
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+<div class="header-login">
+    <div class="w">
+        <div class="logo">
+            <a href="{{ url('/') }}"><img src="/static/img/public/logo.png" alt=""></a>
+            <span class="logo-salutatory">欢迎登陆</span>
+            <div class="logo-link"><a href="javascript:;"><i class="icon-chat"></i>登录页面，调查问卷</a></div>
+        </div>
+    </div>
+</div>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+<div class="content-login" style="background-color: #323234;">
+    <div class="w">
+        <div class="content-activity">
+            <a href="javascript:;"><img src="/static/img/temp/login-activity-001.jpg" alt=""></a>
+        </div>
+        <div class="content-login-form">
+            <div class="content-login-form-header"><h3>账户登陆</h3></div>
+            <div class="content-login-form-main">
+                <div class="msg-wrap">
+                    <div class="msg-error hide"><i class="icon-error"> </i><span>账户名与密码不匹配，请重新输入</span></div>
                 </div>
+                <form action="javascript:;">
+                    <div class="form-item">
+                        <label for="loginform-username"><i class="icon-user"></i></label>
+                        <input type="text" id="loginform-username" placeholder="邮箱/用户名/已验证手机" autocomplete="off">
+                    </div>
+                    <div class="form-item">
+                        <label for="loginform-password"><i class="icon-locked"></i></label>
+                        <input type="password" id="loginform-password" placeholder="密码">
+                    </div>
+                    <div class="form-checkbox">
+                        <input type="checkbox" name="LoginForm[rememberMe]" value="1" checked="checked" id="loginform-checkbox">
+                        <label for="loginform-checkbox">自动登录</label>
+                        <a href="javascript:;">忘记密码</a>
+                    </div>
+                    <div class="form-btn">
+                        <button type="submit">登&nbsp;&nbsp;录</button>
+                    </div>
+                </form>
+            </div>
+            <div class="content-login-form-footer">
+                <ul>
+                    <li class="item"><span>|</span><a href="javascript:;"><i class="icon-qq"></i>QQ</a></li>
+                    <li class="item"><span>|</span><a href="javascript:;"><i class="icon-wechat"></i>微信</a></li>
+                    <li class="link"><a href="register.html"><i class="icon-chevron-circle-right"></i>立即注册</a></li>
+                </ul>
             </div>
         </div>
     </div>
 </div>
-@endsection
+
+<div class="footer-login">
+    <div class="w">
+        <ul>
+            <li><span>|</span><a href="javascript:;">关于我们</a></li>
+            <li><span>|</span><a href="javascript:;">联系我们</a></li>
+            <li><span>|</span><a href="javascript:;">人才招聘</a></li>
+            <li><span>|</span><a href="javascript:;">商家入驻</a></li>
+            <li><span>|</span><a href="javascript:;">广告服务</a></li>
+            <li><span>|</span><a href="javascript:;">友情链接</a></li>
+            <li><span>|</span><a href="javascript:;">帮助中心</a></li>
+        </ul>
+        <p>Copyright © 2015 - 2017  eBestMall 鸿宇科技 版权所有</p>
+    </div>
+</div>
+
+</body>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,139 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="renderer" content="webkit">
+    <title>eBestMall - B2B2C商城系统</title>
+    <meta name="description" content="eBestMall是国内电子商务系统及服务解决方案新创品牌。为传统企业及创业者提供零售网店系统、网上商城系统、分销系统、B2B2C商城系统、微信分销系统、行业ERP等产品和解决方案。">
+    <meta name="keyword" content="eBestMall,鸿宇商城,鸿宇分销,网店系统,网上商城系统,分销系统,B2B2C商城系统,微信分销系统">
+    <link rel="stylesheet" href="/static/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/static/css/app.css" />
+    <link rel="stylesheet" href="/static/css/register.css" />
+<body>
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+<div class="header-register">
+    <div class="w">
+        <div class="logo">
+            <a href="{{ url('/') }}"><img src="static/img/public/logo.png" alt=""></a>
+            <span class="logo-salutatory">欢迎注册</span>
+            <div class="logo-link">已有账号？ <a href="login.html">请登录</a></div>
+        </div>
+    </div>
+</div>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+<div class="content-register">
+    <div class="w">
+        <div class="content-register-from">
+            <form action="javascript:;">
+                <div class="form-item">
+                    <label>用　户　名</label>
+                    <input type="text" placeholder="您的账户名和登录名" autocomplete="off">
                 </div>
+                <div class="input-tip">
+                    <span><i class="icon-error"></i>支持中文、字母、数字、“-”“_”的组合，4-20个字符</span>
+                    <span class="error hide"><i class="icon-error"></i>格式错误，仅支持汉字、字母、数字、“-”“_”的组合</span>
+                </div>
+
+                <div class="form-item">
+                    <label>设 置 密 码</label>
+                    <input type="password" placeholder="建议至少使用两种字符组合">
+                </div>
+                <div class="input-tip">
+                    <span><i class="icon-error"></i>建议至少使用两种字符组合</span>
+                    <span class="error hide"><i class="icon-error"></i></span>
+                </div>
+
+                <div class="form-item">
+                    <label>确 认 密 码</label>
+                    <input type="password" placeholder="请再次输入密码">
+                </div>
+                <div class="input-tip">
+                    <span><i class="icon-error"></i>请再次输入密码</span>
+                    <span class="error hide"><i class="icon-error"></i></span>
+                </div>
+
+                <div class="form-item">
+                    <label>邮 箱 验 证</label>
+                    <input type="text" placeholder="建议使用常用邮箱">
+                </div>
+                <div class="input-tip">
+                    <span><i class="icon-error"></i>建议使用常用邮箱，完成验证后，你可以用该邮箱登录和找回密码</span>
+                    <span class="error hide"><i class="icon-error"></i></span>
+                </div>
+
+                <div class="form-item">
+                    <label>中国&nbsp;&nbsp;+86</label>
+                    <input type="text" placeholder="建议使用常用手机">
+                </div>
+                <div class="input-tip">
+                    <span><i class="icon-error"></i>建议使用常用手机，完成验证后，你可以用该手机登录和找回密码</span>
+                    <span class="error hide"><i class="icon-error"></i></span>
+                </div>
+
+                <div class="form-item">
+                    <label>验　证　码</label>
+                    <input type="text" placeholder="请输入验证码">
+                </div>
+                <div class="input-tip">
+                    <span><i class="icon-error"></i>看不清？点击图片更换验证码</span>
+                    <span class="error hide"><i class="icon-error"></i></span>
+                </div>
+
+                <div class="form-item">
+                    <label>手机验证码</label>
+                    <input type="text" placeholder="请输入手机验证码">
+                </div>
+                <div class="input-tip">
+                    <span><i class="icon-error"></i></span>
+                </div>
+
+                <div class="form-agreement">
+                    <div>
+                        <input type="checkbox" name="agreement" checked="">阅读并同意<a href="javascript:;">《用户注册协议》</a>
+                    </div>
+                    <div class="input-tip"><span></span></div>
+                </div>
+                <div class="input-tip"><span></span></div>
+                <div class="form-btn">
+                    <button type="submit">立即注册</button>
+                </div>
+
+            </form>
+
+            <div class="content-register-from-link">
+                <div class="content-register-from-link-third-party-qq content-register-from-link-third-party">
+                    <a href="javascript:;">
+                        <i class="icon-qq"></i>
+                        <span>QQ 用户注册</span>
+                    </a>
+                </div>
+                <div class="content-register-from-link-third-party-wechat content-register-from-link-third-party">
+                    <a href="javascript:;">
+                        <i class="icon-wechat"></i>
+                        <span>微信用户注册</span>
+                    </a>
+                </div>
+                <div class="content-register-from-link-third-party-phone-fast-reg"></div>
             </div>
         </div>
     </div>
 </div>
-@endsection
+
+<div class="footer-register">
+    <div class="w">
+        <ul>
+            <li><span>|</span><a href="javascript:;">关于我们</a></li>
+            <li><span>|</span><a href="javascript:;">联系我们</a></li>
+            <li><span>|</span><a href="javascript:;">人才招聘</a></li>
+            <li><span>|</span><a href="javascript:;">商家入驻</a></li>
+            <li><span>|</span><a href="javascript:;">广告服务</a></li>
+            <li><span>|</span><a href="javascript:;">友情链接</a></li>
+            <li><span>|</span><a href="javascript:;">帮助中心</a></li>
+        </ul>
+        <p>Copyright © 2015 - 2017  eBestMall 鸿宇科技 版权所有</p>
+    </div>
+</div>
+</body>
+</html>
