90b57b7872a9366f11d692d5e51c1de9261579f9
Shadow <admin@hongyuvip.com>
2017-09-03 15:32:54 +0800

    eBestMall - Laravel 5.5 LTS

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         return view('user.index');
