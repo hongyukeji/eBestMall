9a4516227ede2a2557c672d3ce5dffd219ecb3ae
Shadow <admin@hongyuvip.com>
2017-09-03 11:24:23 +0800

    eBestMall - Laravel 5.5 LTS

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -24,5 +24,29 @@ class UsersTableSeeder extends Seeder
                 'updated_at' => '2017-08-16 05:29:16',
             ],
         ]);
+
+        DB::table('users_info')->insert([
+            [
+                'user_id' => '1',
+                'nickname' => '影子',
+                'score' => '99999999',
+                'money' => '99999999.99',
+                'payment_password' => bcrypt('123456'),
+                'real_name' => '张鸿宇',
+                'id_card_no' => '320324198808181288',
+                'age' => '28',
+                'sex' => '1',
+                'birthday' => '1988-08-18',
+                'qq' => '1527200768',
+                'ww' => '',
+                'address' => '',
+                'register_ip' => '127.0.0.1',
+                'login_number' => '7',
+                'last_login_ip' => '127.0.0.1',
+                'last_login_time' => '00000000000',
+                'created_at' => '2017-08-08 05:29:16',
+                'updated_at' => '2017-08-16 05:29:16',
+            ],
+        ]);
     }
 }
