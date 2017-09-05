a8056e4ae0bd6314adcfc9ffd4027e317acf4fff
Shadow <admin@hongyuvip.com>
2017-09-05 19:10:17 +0800

    eBestMall - Laravel 5.5 LTS

--- database/migrations/2017_09_05_133934_create_goods_categories_table.php
+++ database/migrations/2017_09_05_133934_create_goods_categories_table.php
@@ -17,9 +17,9 @@ class CreateGoodsCategoriesTable extends Migration
             $table->increments('id');
             $table->integer('cat_id')->comment('分类id');
             $table->string('cat_name')->comment('分类名称');
+            $table->string('cat_desc')->comment('描述');
             $table->integer('parent_id')->comment('分类父id');
-            $table->string('keywords')->comment('关键字');
-            $table->string('description')->comment('描述');
+            $table->string('cat_pic')->comment('分类图片');
             $table->tinyInteger('sort_order')->comment('排序次序');
             $table->tinyInteger('is_show')->comment('是否显示');
             $table->tinyInteger('status')->comment('状态');
