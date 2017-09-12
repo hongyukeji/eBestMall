1654c2c6015d7466a01ae46b36970f544bf8648e
Shadow <admin@hongyuvip.com>
2017-09-12 14:52:43 +0800

    eBestMall - Yii2.0

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=ebestmall',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '123456',
     'charset' => 'utf8',
 ];
