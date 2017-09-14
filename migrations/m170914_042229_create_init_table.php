cdfed841a9c06eedd33b874693e3b06956cfe094
Shadow <admin@hongyuvip.com>
2017-09-14 12:25:46 +0800

    eBestMall - Yii2.0

--- migrations/m170914_042229_create_init_table.php
+++ migrations/m170914_042229_create_init_table.php
@@ -7,14 +7,22 @@ use yii\db\Migration;
  */
 class m170914_042229_create_init_table extends Migration
 {
+    const TABLE_NAME = '{{%init}}';
+    const TABLE_NAME_TAB = '初始化';
+
     /**
      * @inheritdoc
      */
     public function up()
     {
-        $this->createTable('init', [
-            'id' => $this->primaryKey(),
-        ]);
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB COMMENT=' . "'" . self::TABLE_NAME_TAB . "'";
+        }
+
+        $this->createTable(self::TABLE_NAME, [
+            'id' => $this->primaryKey()->comment('自增ID'),
+        ], $tableOptions);
     }
 
     /**
@@ -22,6 +30,6 @@ class m170914_042229_create_init_table extends Migration
      */
     public function down()
     {
-        $this->dropTable('init');
+        $this->dropTable(self::TABLE_NAME);
     }
 }
