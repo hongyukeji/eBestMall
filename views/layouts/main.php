04c8ba79b27654399bfb286e094c822fb4f428f0
Shadow <admin@hongyuvip.com>
2017-09-14 10:41:08 +0800

    eBestMall - Yii2.0

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -28,7 +28,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => 'eBestMall',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
--- views/site/index.php
+++ views/site/index.php
@@ -2,7 +2,7 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'eBestMall';
 ?>
 <div class="site-index">
 
