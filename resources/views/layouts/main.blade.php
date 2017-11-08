76e70d9506d007092e308d3d54c5127f34918023
HongYuKeJi <hongyukeji@126.com>
2017-11-08 23:20:58 +0800

    eBestMall

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="keyword" content="eBestMall,鸿宇商城,鸿宇分销,网店系统,网上商城系统,分销系统,B2B2C商城系统,微信分销系统">
     <meta name="description" content="eBestMall是国内电子商务系统及服务解决方案新创品牌。为传统企业及创业者提供零售网店系统、网上商城系统、分销系统、B2B2C商城系统、微信分销系统、行业ERP等产品和解决方案。">
-    <meta name="author" content="{{ config('system.name') }}-{{ config('system.version') }} {{ config('system.url') }}">
+    <meta name="author" content="鸿宇科技">
 
     @if(Request::path() == '/')
     <title>eBestMall - B2B2C商城系统</title>
