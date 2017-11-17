01383161d05e02d632e45b44d006889b3345dc68
HongYuKeJi <hongyukeji@126.com>
2017-11-17 23:03:09 +0800

    eBestMall Init

---
+++ environments/prod/common/config/main-local.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=ebestmall',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'tablePrefix' => 'ebm_',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+        ],
+    ],
+];
