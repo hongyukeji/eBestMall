cc54c067523ca214fc8c62513c56a5cb2959bba0
HongYuKeJi <hongyukeji@126.com>
2017-11-17 21:08:31 +0800

    eBestMall Init

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -7,6 +7,7 @@ return [
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
+            'tablePrefix' => 'ebm_',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
