6ba144abdd22a1f1bf4bb31d5a9605b06bed71be
HongYuKeJi <hongyukeji@126.com>
2017-11-20 12:45:24 +0800

    eBestMall Init

--- themes/basic/site/index.php
+++ themes/basic/site/index.php
@@ -194,7 +194,7 @@ $this->title = 'eBestMall（鸿宇V8） - B2B2C商城系统';
 
                 <div class="content-sec-kill-hd-col-right">
                     <span class="sk_cd_tip">当前场次</span>
-                    <div class="sk_cd_main sec-kill-time" data-time="2017-11-20 12:38:00">
+                    <div class="sk_cd_main sec-kill-time" data-time="2017-12-18 00:00:00">
                         <div class="sec-kill-day-wrap">
                             <div class="cd_item cd_day"><div class="cd_item_txt">00</div></div>
                             <div class="cd_split_text">天</div>
