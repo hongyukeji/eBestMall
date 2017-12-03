5e9f54e87ec60cfeda2c3076603671fcd0b53f98
HongYuKeJi <hongyukeji@126.com>
2017-12-03 19:42:54 +0800

    eBestMall

--- modules/admin/views/layouts/content.php
+++ modules/admin/views/layouts/content.php
@@ -37,10 +37,9 @@ use dmstr\widgets\Alert;
 
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        <b>Version</b> 2.0
+        <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
-    reserved.
+    <strong>Copyright &copy; 2015-<?= date('Y') ?> <a href="http://www.hongyuvip.com">HongYuKeJi</a>.</strong> All rights reserved.
 </footer>
 
 <!-- Control Sidebar -->
