5df531e3d7e6c64bc6de1b0f6d3d9f00ce03e0c8
HongYuKeJi <hongyukeji@126.com>
2017-12-03 18:17:31 +0800

    eBestMall

--- migrations/m171203_100957_create_admin_table.php
+++ migrations/m171203_100957_create_admin_table.php
@@ -2,25 +2,28 @@
 
 use yii\db\Migration;
 
-class m130524_201442_init extends Migration
+class m171203_100957_create_admin_table extends Migration
 {
+    const TABLE_NAME = '{{%admin}}';
+    const TABLE_NAME_TAB = '管理员';
+
     public function up()
     {
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
-            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB COMMENT=' . "'" . self::TABLE_NAME_TAB . "'";
         }
 
-        $this->createTable('{{%user}}', [
-            'user_id' => $this->primaryKey(),
+        $this->createTable(self::TABLE_NAME, [
+            'admin_id' => $this->primaryKey(),
             'username' => $this->string()->notNull()->unique(),
             'auth_key' => $this->string(32)->notNull(),
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
             'email' => $this->string()->notNull()->unique(),
             'avatar' => $this->string()->notNull()->defaultValue(''),
-            'status' => $this->smallInteger()->notNull()->defaultValue(1),
+            'sort_order' => $this->integer()->notNull()->defaultValue(0)->comment('排序'),
+            'status' => $this->smallInteger()->notNull()->defaultValue(0)->comment('状态'),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
         ], $tableOptions);
@@ -28,6 +31,6 @@ class m130524_201442_init extends Migration
 
     public function down()
     {
-        $this->dropTable('{{%user}}');
+        $this->dropTable(self::TABLE_NAME);
     }
 }
