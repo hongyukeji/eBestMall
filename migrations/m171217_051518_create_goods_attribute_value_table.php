fae3f3c6fcc00e8c0874390bd61f62fdc87129cb
HongYuKeJi <hongyukeji@126.com>
2017-12-17 13:16:27 +0800

    eBestMall

---
+++ migrations/m171217_051518_create_goods_attribute_value_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+class m171217_051518_create_goods_attribute_value_table extends Migration
+{
+    const TABLE_NAME = '{{%goods_attribute_value}}';
+    const TABLE_NAME_TAB = '商品属性值表';
+
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB COMMENT=' . "'" . self::TABLE_NAME_TAB . "'";
+        }
+
+        $this->createTable(self::TABLE_NAME, [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull()->comment('名称'),
+            'sort_order' => $this->integer()->notNull()->defaultValue(0)->comment('排序'),
+            'status' => $this->smallInteger()->notNull()->defaultValue(0)->comment('状态'),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        $this->dropTable(self::TABLE_NAME);
+    }
+}
