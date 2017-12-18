5c5c04962cee7c7cce8ef6516346ea77f41584e1
HongYuKeJi <hongyukeji@126.com>
2017-12-18 18:04:51 +0800

    eBestMall

--- migrations/m130524_201442_init.php
+++ migrations/m130524_201442_init.php
@@ -9,7 +9,7 @@ class m130524_201442_init extends Migration
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB AUTO_INCREMENT=10000';
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB AUTO_INCREMENT=100000';
         }
 
         $this->createTable('{{%user}}', [
