9e34c6aa0caa3a3de9c55b4dda707710b0c6757e
HongYuKeJi <hongyukeji@126.com>
2017-12-18 17:56:44 +0800

    eBestMall

--- migrations/m130524_201442_init.php
+++ migrations/m130524_201442_init.php
@@ -9,7 +9,7 @@ class m130524_201442_init extends Migration
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB AUTO_INCREMENT=10000';
         }
 
         $this->createTable('{{%user}}', [
@@ -18,11 +18,11 @@ class m130524_201442_init extends Migration
             'auth_key' => $this->string(32)->notNull(),
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
-            'email' => $this->string()->notNull(),
-            'mobile_phone' => $this->string(32)->notNull()->unique(),
+            'email' => $this->string(),
+            'mobile_phone' => $this->string(32)->unique(),
             'payment_password' => $this->string()->comment('支付密码'),
-            'avatar' => $this->string(),
-            'nickname' => $this->string()->notNull()->unique()->comment('用户昵称'),
+            'avatar' => $this->string()->comment('头像'),
+            'nickname' => $this->string()->unique()->comment('用户昵称'),
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
