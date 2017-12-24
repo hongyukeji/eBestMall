d2119f6428da1df59ade2d54c883d702681d2e8f
HongYuKeJi <hongyukeji@126.com>
2017-12-25 01:14:12 +0800

    eBestMall

--- components/authclient/clients/QqAuth.php
+++ components/authclient/clients/QqAuth.php
@@ -164,4 +164,11 @@ class QqAuth extends OAuth2
         $response->setContent($content);
     }
 
+    protected function defaultViewOptions()
+    {
+        return [
+            'popupWidth' => 860,
+            'popupHeight' => 480,
+        ];
+    }
 }
