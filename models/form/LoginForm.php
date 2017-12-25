37e32234a6ceb973d1500e610f0163a9e6b31293
HongYuKeJi <hongyukeji@126.com>
2017-12-25 11:35:56 +0800

    eBestMall

--- models/form/LoginForm.php
+++ models/form/LoginForm.php
@@ -67,7 +67,7 @@ class LoginForm extends Model
     public function login()
     {
         if ($this->validate()) {
-            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 : 0);
+            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? Yii::$app->params['user.rememberMeDuration'] : 0);
         }
         return false;
     }
