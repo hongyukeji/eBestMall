a8e5f3e817e34ba3dc1cf04db859ed3f25ab8746
Shadow <admin@hongyuvip.com>
2018-02-22 16:07:41 +0800

    eBestMall-Laravel

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1402,7 +1402,7 @@
                     <div class="content-sec-kill-hd-col-right">
                         <div class="sec-kill-count-down-wrap">
                             <span class="sk_cd_tip">当前场次</span>
-                            <div class="sk_cd_main sec-kill-time" data-time="2018-02-16 00:00:00">
+                            <div class="sk_cd_main sec-kill-time" data-time="2018-03-18 00:00:00">
                                 <div class="sec-kill-day-wrap">
                                     <div class="cd_item cd_day"><div class="cd_item_txt">00</div></div>
                                     <div class="cd_split_text">天</div>
