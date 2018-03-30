b10c7aac385a762c278e3868833798623b675137
Shadow <admin@hongyuvip.com>
2018-03-30 12:13:31 +0800

    Initial commit

---
+++ src/main/java/com/ebestmall/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.ebestmall.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "eBestMall Greetings from Spring Boot!";
+    }
+    
+}
