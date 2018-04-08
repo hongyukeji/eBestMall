f4fcbbdbd81bd34353fe5b59b51de73f60615b84
Shadow <admin@hongyuvip.com>
2018-04-08 15:42:24 +0800

    eBestMall Laravel Initial commit

--- ebm.bat
+++ ebm.bat
@@ -3,23 +3,25 @@
 rem -------------------------------------------------------------
 rem  HongYuKeJi command line bootstrap script for Windows.
 rem
-rem  @author Shadow <admin@hongyuvip.com>
+rem  @author HongYuKeJi <admin@hongyuvip.com>
 rem  @link http://www.hongyuvip.com/
 rem  @copyright Copyright (c) 2018 HongYu Software LLC
-rem  @license http://www.hongyuvip.com/license/
+rem  @license http://www.hongyuvip.com/
 rem -------------------------------------------------------------
 
+rem -------------------------------------------------------------
 rem       __________                 __     _____         .__  .__
 rem   ____\______   \ ____   _______/  |_  /     \ _____  |  | |  |
 rem _/ __ \|    |  _// __ \ /  ___/\   __\/  \ /  \\__  \ |  | |  |
 rem \  ___/|    |   \  ___/ \___ \  |  | /    Y    \/ __ \|  |_|  |__
 rem  \___  >______  /\___  >____  > |__| \____|__  (____  /____/____/
 rem      \/       \/     \/     \/               \/     \/
+rem -------------------------------------------------------------
 
 @setlocal
 
 if "%PHP_COMMAND%" == "" set PHP_COMMAND=php.exe
 
-"%PHP_COMMAND%" "yii" %*
+"%PHP_COMMAND%" "artisan" %*
 
 @endlocal
