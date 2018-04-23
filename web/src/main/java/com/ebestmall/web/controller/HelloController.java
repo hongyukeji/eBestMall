4518173222acaa05090f88b5f27129912d6cbf8b
Shadow <admin@hongyuvip.com>
2018-04-23 12:11:32 +0800

    Init

---
+++ web/src/main/java/com/ebestmall/web/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.ebestmall.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "eBestMall hello world!";
+    }
+}
