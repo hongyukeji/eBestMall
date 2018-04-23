e0726efd2f7808ede9a9541902ed71196c018f36
Shadow <admin@hongyuvip.com>
2018-04-23 15:08:08 +0800

    Init

--- web/src/main/java/com/ebestmall/web/controller/MainController.java
+++ web/src/main/java/com/ebestmall/web/controller/MainController.java
@@ -15,34 +15,42 @@ public class MainController {
     public String article() {
         return "article/index";
     }
+
     @GetMapping("auth/login")
     public String authLogin() {
         return "auth/login";
     }
+
     @GetMapping("auth/register")
     public String authRegister() {
         return "auth/register";
     }
+
     @GetMapping("cart")
     public String cart() {
         return "cart/index";
     }
+
     @GetMapping("cart/confirm")
     public String cartConfirm() {
         return "cart/confirm";
     }
+
     @GetMapping("cart/create")
     public String cartCreate() {
         return "cart/create";
     }
+
     @GetMapping("goods")
     public String goods() {
         return "goods/index";
     }
+
     @GetMapping("search")
     public String search() {
         return "search/index";
     }
+
     @GetMapping("user")
     public String user() {
         return "user/index";
