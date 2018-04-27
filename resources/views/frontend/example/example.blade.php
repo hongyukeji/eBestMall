ca921d85af1109208440815a4bb27d891b5ff91e
Shadow <admin@hongyuvip.com>
2018-04-28 00:40:27 +0800

    Init

---
+++ resources/views/frontend/example/example.blade.php
@@ -0,0 +1,17 @@
+@extends('frontend.layouts.app')
+
+@section('title')
+    Example - eBestMall
+@stop
+
+@section('stylesheet')
+    <link rel="stylesheet" href="{{ asset('static/css/example.css') }}">
+@stop
+
+@section('javascript')
+    <script src="{{ asset('static/js/example.js') }}"></script>
+@stop
+
+@section('content')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,15 @@
 /* Frontend */
 Route::namespace('Frontend')->group(function () {
     Route::get('/', 'MainController@index')->name('frontend.index');
+    Route::get('/article', function () {return view('frontend.article.index');});
+    Route::get('/search', function () {return view('frontend.search.index');});
+    Route::get('/user', function () {return view('frontend.user.index');});
+    Route::get('/goods', function () {return view('frontend.goods.index');});
+    Route::get('/cart', function () {return view('frontend.cart.index');});
+    Route::get('/cart/create', function () {return view('frontend.cart.create');});
+    Route::get('/cart/confirm', function () {return view('frontend.cart.confirm');});
+    Route::get('/auth/login', function () {return view('frontend.auth.login');});
+    Route::get('/auth/register', function () {return view('frontend.auth.register');});
 });
 
 /* Backend */
