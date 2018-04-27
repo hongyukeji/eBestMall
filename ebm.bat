cde8e5279225e90e035df9681a640df6e1fee04e
Shadow <admin@hongyuvip.com>
2018-04-27 21:01:15 +0800

    Init

---
+++ ebm.bat
@@ -0,0 +1,27 @@
+@echo off
+
+rem -------------------------------------------------------------
+rem  HongYuKeJi command line bootstrap script for Windows.
+rem
+rem  @author HongYuKeJi <admin@hongyuvip.com>
+rem  @link http://www.hongyuvip.com/
+rem  @copyright Copyright (c) 2018 HongYu Software LLC
+rem  @license http://www.hongyuvip.com/
+rem -------------------------------------------------------------
+
+rem -------------------------------------------------------------
+rem       __________                 __     _____         .__  .__
+rem   ____\______   \ ____   _______/  |_  /     \ _____  |  | |  |
+rem _/ __ \|    |  _// __ \ /  ___/\   __\/  \ /  \\__  \ |  | |  |
+rem \  ___/|    |   \  ___/ \___ \  |  | /    Y    \/ __ \|  |_|  |__
+rem  \___  >______  /\___  >____  > |__| \____|__  (____  /____/____/
+rem      \/       \/     \/     \/               \/     \/
+rem -------------------------------------------------------------
+
+@setlocal
+
+if "%PHP_COMMAND%" == "" set PHP_COMMAND=php.exe
+
+"%PHP_COMMAND%" "artisan" %*
+
+@endlocal
