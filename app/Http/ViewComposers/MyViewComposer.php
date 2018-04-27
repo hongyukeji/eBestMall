8dc4c8a8cfebcde55db058a8c2a54300b62cb57a
Shadow <admin@hongyuvip.com>
2018-04-28 00:55:16 +0800

    Init

---
+++ app/Http/ViewComposers/MyViewComposer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * eBestMall
+ * ============================================================================
+ * Copyright © 2015-2018 HongYuKeJi.Co.Ltd. All rights reserved.
+ * Http://www.hongyuvip.com
+ * ----------------------------------------------------------------------------
+ * 仅供学习交流使用，如需商用请购买商用版权。
+ * 堂堂正正做人，踏踏实实做事。
+ * ----------------------------------------------------------------------------
+ * Author: Shadow  QQ: 1527200768  Time: 2018/4/28 0:47
+ * E-mail: admin@hongyuvip.com
+ * ============================================================================
+ */
+
+namespace App\Http\ViewComposers;
+
+use Illuminate\Contracts\View\View;
+
+class MyViewComposer
+{
+    public function compose(View $view)
+    {
+        $view->with('key','value');
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,6 +16,18 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        //View::share('key', 'value');    // 所有视图中共享数据
+
+        // 指定视图中注入数据
+        //View::composer('frontend.layouts.app', 'App\Http\ViewComposers\MyViewComposer');
+        //View::composer(['navigation', 'footer'], 'App\Http\ViewComposers\MyViewComposer');
+        View::composers([
+            'App\Http\ViewComposers\MyViewComposer' => ['frontend.layouts.app'],
+            //'App\Http\ViewComposers\AdminComposer' => ['admin.index', 'admin.profile'],
+            //'App\Http\ViewComposers\UserComposer' => 'user',
+            //'App\Http\ViewComposers\GoodsComposer' => 'goods'
+        ]);
     }
 
     /**
