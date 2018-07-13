4f3fa2e15cf5e7e59af005fc4a7577e534d60b47
Shadow <admin@hongyuvip.com>
2018-07-13 13:16:12 +0800

    Init

--- common/config/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-Yii::setAlias('@common', dirname(__DIR__));
-Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
-Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
-Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
-Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
-Yii::setAlias('@mobile', dirname(dirname(__DIR__)) . '/mobile');
